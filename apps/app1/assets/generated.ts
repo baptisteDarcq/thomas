29e24f2b68fac3c6a960740e5fabed46c8e0605e
Baptiste Darcq <baptiste.darcq@matters.tech>
2025-02-06 11:31:55 +0100

    setup-fonts-part-4

--- apps/app1/assets/generated.ts
+++
@@ -1,6 +0,0 @@
-export const assets = {
-  "images/adaptive-icon": require("./images/adaptive-icon.png"),
-  "images/icon": require("./images/icon.png"),
-  "images/logo": require("./images/logo.png"),
-  "images/splash-icon": require("./images/splash-icon.png")
-};
--- apps/app2/assets/generated.ts
+++
@@ -1,9 +0,0 @@
-export const assets = {
-  "fonts/Primary-Bold": require("./fonts/Primary-Bold.ttf"),
-  "fonts/Primary-Light": require("./fonts/Primary-Light.ttf"),
-  "fonts/Primary-Regular": require("./fonts/Primary-Regular.ttf"),
-  "images/adaptive-icon": require("./images/adaptive-icon.png"),
-  "images/favicon": require("./images/favicon.png"),
-  "images/icon": require("./images/icon.png"),
-  "images/splash-icon": require("./images/splash-icon.png")
-};
