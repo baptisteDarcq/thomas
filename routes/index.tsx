f548f3a42f472fd2a45e0355b68ae8a5781505e0
Thomas Viaud <thomas@ridemotto.com>
2025-02-06 12:08:00 +0100

    i18n working 3

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,6 @@
 import { Href, Redirect } from "expo-router";
 import Constants from "expo-constants";
-import "src/core/translations"
 
 export default function Layout() {
-  return <Redirect href={Constants.expoConfig.extra.initialRoute as Href} />;
+  return <Redirect href={Constants.expoConfig?.extra?.initialRoute as Href} />;
 }
